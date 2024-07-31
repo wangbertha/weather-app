a6a72e822602682aaed62733042596c87f84c97c
wangbertha <wangbertha@gmail.com>
2024-07-31 16:34:29 -0500

    Initialize boilerplate code
    Streamline development with Webpack JavaScript module bundler

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# weather-app
+# Weather App
+
+Note: Favicon weather icon credited to iconixar (Shared on Flaticon: https://www.flaticon.com/free-icon/cloudy_1163661?term=weather&page=1&position=1&origin=search&related_id=1163661)
---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Weather App',
+        }),
+        new FaviconsWebpackPlugin('./src/assets/cloudy.png'),
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
