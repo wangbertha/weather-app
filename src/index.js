7c6a9074d56ae61d49948b28f3759918edf85bc2
wangbertha <wangbertha@gmail.com>
2024-08-01 10:21:41 -0500

    Fetch 15-day forecast from Visual Crossing API
    Forecast data will be the main information conveyed for the user

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('test');
+const key = '3L63BZALNF6WDMKYETGMT6Q4U';
+
+async function getWeatherData(myLocation) {
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?locations=${myLocation}&aggregateHours=24&unitGroup=us&shortColumnNames=false&contentType=json&key=${key}`;
+    try {
+        const response = await fetch(url);
+        const respJson = await response.json();
+        const data = respJson.locations[myLocation].values;
+        return data;
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+let testLocation = 'Chicago,IL';
+getWeatherData(testLocation);
